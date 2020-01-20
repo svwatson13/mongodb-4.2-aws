d28bd2f23f0fd0c3bf64f044590f5bf971e935b0
Sam Watson <SWatson@spartaglobal.com>
2020-01-20 11:32:17 +0000

    DOD

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# Running tests on mongodb aws server
 
+# To run kitchen.cloud.yml File
+KITCHEN_YAML=kitchen.cloud.yml kitchen verify
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,15 +6,13 @@
 include_recipe 'sc-mongodb'
 include_recipe 'apt'
 
-
-
 template '/etc/mongod.conf' do
    source 'mongod.conf.erb'
    variables bindIp: node['mongodb']['config']['mongod']['net']['bindIp'],
    port: node['mongodb']['config']['mongod']['net']['port']
    notifies :restart, 'service[mongod]'
 end
-  #resource link
+
 template "/lib/systemd/system/mongod.service" do
   source "mongod.service.erb"
   notifies :restart, 'service[mongod]'
